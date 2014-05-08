582729028cda821385ab6504df95708ac46e1f9e
Wes Morgan <cap10morgan@gmail.com>
2014-05-08 12:35:16 -0600

    [WM JE] add some tests for the consume-messages fn

--- test/democracyworks/squishy_test.clj
+++ test/democracyworks/squishy_test.clj
@@ -8,12 +8,35 @@
     (let [result (atom [])]
       (with-redefs [squishy/get-queue (constantly nil)
                     sqs/delete (constantly nil)
-                    sqs/polling-receive
-                    (fn [client q & opts]
-                      ["msg 1" "msg 2"])]
-        (is (= '("MSG 1" "MSG 2")
+                    sqs/polling-receive (constantly ["msg 1" "msg 2"])]
+        (is (= ["MSG 1" "MSG 2"]
                (do
-                @(consume-messages nil
-                                   #(swap! result conj
-                                    (clojure.string/upper-case %)))
-                @result)))))))
+                 @(consume-messages nil
+                                    #(swap! result conj
+                                            (clojure.string/upper-case %)))
+                 @result))))))
+
+  (testing "deletes msgs as it consumes them"
+    (let [queue (atom ["msg 1" "msg 2"])]
+      (with-redefs [squishy/get-queue (constantly nil)
+                    sqs/polling-receive (constantly @queue)
+                    sqs/delete (fn [client message]
+                                 (swap! queue #(drop 1 %)))]
+        (is (= []
+               (do
+                 @(consume-messages nil (constantly nil))
+                 @queue))))))
+
+  (testing "puts msgs on fail queue when processing fn throws exception"
+    (let [fail-queue (atom [])]
+      (with-redefs [squishy/get-queue (constantly nil)
+                    squishy/get-fail-queue (constantly nil)
+                    sqs/polling-receive (constantly [{:body "msg 1"}
+                                                     {:body "msg 2"}])
+                    sqs/delete (constantly nil)
+                    sqs/send (fn [client q msg] (swap! fail-queue conj msg))]
+        (is (= ["{:body \"msg 1\", :error \"msg 1 failed\"}"
+                "{:body \"msg 2\", :error \"msg 2 failed\"}"]
+               (do @(consume-messages
+                     nil #(throw (Exception. (str (:body %) " failed"))))
+                   @fail-queue)))))))
