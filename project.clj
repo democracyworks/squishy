ebf5f1c9f778be53af9915d73e2e92197bafe989
Wes Morgan <cap10morgan@gmail.com>
2014-05-12 09:39:38 -0600

    release v1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks.squishy "1.0.0-SNAPSHOT"
+(defproject democracyworks.squishy "1.0.0"
   :description "A library for consuming Amazon SQS queue messages"
   :url "http://github.com/turbovote/squishy"
   :license {:name "Eclipse Public License"
