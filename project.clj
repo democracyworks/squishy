594a3cdf8e24e7744a7cb4ddd1e89f1c6a2631c9
Chris Shea <cmshea@gmail.com>
2015-09-15 15:21:18 -0400

    [CS] Update to Clojure 1.7.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/turbovote/squishy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.amazonaws/aws-java-sdk "1.7.8.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [org.slf4j/slf4j-simple "1.7.7"]
