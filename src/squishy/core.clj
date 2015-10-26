0a043cc9b66dba2f0fa0181ea0872e792064a702
Eric Normand <ericwnormand@gmail.com>
2015-10-26 13:17:11 -0500

    [EN] Add some resiliency by looping and retrying

--- src/squishy/core.clj
+++ src/squishy/core.clj
@@ -15,19 +15,28 @@
 (defn safe-process [client fail-queue-url f]
   (fn [message]
     (log/debug "Processing SQS message:" (str "<<" message ">>"))
-    (try (f message)
-      (catch Exception e
+    (try
+      (f message)
+      (catch Throwable e
         (let [body (:body message)]
           (log/error "Failed to process" body e)
           (report-error client fail-queue-url body e))))))
 
 (defn consume-messages
-  [client queue-name fail-queue-name f]
-  (let [queue-url (sqs/create-queue client queue-name)
-        fail-queue-url (sqs/create-queue client fail-queue-name)]
-    (future
-      (do
-        (log/info "Consuming SQS messages from" queue-url)
-        (dorun
-         (map (sqs/deleting-consumer client (safe-process client fail-queue-url f))
-              (sqs/polling-receive client queue-url :max-wait Long/MAX_VALUE :limit 10)))))))
+  [creds queue-name fail-queue-name f]
+  (future
+    (loop []
+      (let [client (client (:access-key creds)
+                           (:access-secret creds)
+                           (:region creds))
+            queue-url (sqs/create-queue client queue-name)
+            fail-queue-url (sqs/create-queue client fail-queue-name)
+            consume (sqs/deleting-consumer client (safe-process client fail-queue-url f))]
+        (when (try
+                (log/info "Consuming SQS messages from" queue-url)
+                (doseq [message (sqs/polling-receive client queue-url :max-wait Long/MAX_VALUE :limit 10)]
+                  (consume message))
+                (catch Throwable t
+                  (log/error "Failed to consume-messages" t)
+                  :keep-trying))
+          (recur))))))
