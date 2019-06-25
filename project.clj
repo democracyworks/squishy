377e5e4c937922b1d368ff83628838cc13cb9e1e
Troy Hill <troy@democracy.works>
2019-06-25 09:32:59 -0600

    Handle more region formats
    * expand the data-reader tag to take either string
    format regions commonly appear as (`US_EAST_1` and
    `us-east-1`)
    * coerce the `:region` in the `start-consumer` creds
    into a Region instance if it's one of the string
    formats

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/squishy "3.0.3-SNAPSHOT"
+(defproject democracyworks/squishy "3.1.0-SNAPSHOT"
   :description "A library for consuming Amazon SQS queue messages"
   :url "https://github.com/democracyworks/squishy"
   :license {:name "Eclipse Public License"
