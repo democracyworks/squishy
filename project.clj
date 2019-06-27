0dee72455186f7faed7e43e5c939b920f41434de
Troy Hill <troy@democracy.works>
2019-06-27 10:09:56 -0600

    lein-release plugin: preparing 3.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/squishy "3.1.0-SNAPSHOT"
+(defproject democracyworks/squishy "3.1.0"
   :description "A library for consuming Amazon SQS queue messages"
   :url "https://github.com/democracyworks/squishy"
   :license {:name "Eclipse Public License"
